ab458e833954638ac6d33893d830f6f3688b3834
Kaito2008 <Kurankaname@126.com>
2018-02-25 19:01:02 -0800

    Direction
    Direction

--- app/main.py
+++ app/main.py
@@ -47,18 +47,37 @@ def start():
 	# TODO: Do things with data
 
 	return {
-		'color': '#00FF00',
+		'color': '#ffaa00',
 		'taunt': '{} ({}x{})'.format(game_id, board_width, board_height),
 		'head_url': head_url,
 		'name': 'nicksnek',
 		'head_type': 'fang',
 		'tail_type': 'block-bum'
 	}
+    # Board Layout of a 6x6 board
+    # --------------------------------
+    # |(0,0)                    (5,0)|
+    # |(0,1)                    (5,1)|
+    # |                              |
+    # |                              |
+    # |                              |
+    # |(0,5)                    (5,5)|
+    # --------------------------------
+
+	# Strategy:
+    # Run in circles in the center of the board until HP runs to the threshold minimum. (Chase own tail)
+    # *Calculate the distance from the snake to the food by using the formula (dy2-dy1)/(dx2-dx1)   Dy2,Dx2 is the food and Dy1,Dx1 is the snake head
+    # Calculate other snake's distance to the food using the same above formula
+    # Find out if a block is in the way in the next tile
+    # Go for the food
+    # Repeat * again until get the food
+    # Return to center of the board
 
 
 @bottle.post('/move')
 def move():
 	data = bottle.request.json
+	snek, grid = init(data)
 	game_id = data.get('game_id')
 	board_width = data.get('width')
 	board_height = data.get('height')
@@ -70,9 +89,15 @@ def move():
 	above_headx, above_heady = get_up(mysnake_head)
 	directions = ['up', 'down', 'left', 'right']
 	#direction = random.choice(directions)
-	direction = 'up'
-	if (mysnake_head['y'] == 0):
+
+	if (mysnake_head['y'] == 0 and mysnake_head['x'] == 0):
+		direction = 'down'
+	elif (mysnake_head['y'] == board_height and mysnake_head['x'] == board_width):
+		direction = 'up'
+	elif (mysnake_head['y'] == 0):
 		direction = 'left'
+	elif (mysnake_head['x'] == 0):
+		direction = 'right'
 	return {
 		'move': direction,
 		'taunt': 'dat is not de wae'

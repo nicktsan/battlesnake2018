7101c8b8fa168bbde4a085c2c49d46a995509958
nicktsan <nicktsang.manhay@gmail.com>
2018-02-26 21:24:21 -0800

    Implemented check_between
    Implemented check_between. It returns True if there is a cut-off in between two points, and False if not.

--- app/battlesnake_functions.py
+++ app/battlesnake_functions.py
@@ -94,21 +94,24 @@ def calc_distance(x1, y1, x2, y2):
 	vertical_distance = y1 - y2
 	total_distance = abs(horizontal_distance) + abs(vertical_distance)
 	return total_distance
-"""
+
+def dfs():
+	return False
 #returns False if nothing is in between the shortest path from point 1 to point 2.
 #returns True if something is in between the shortest path from point 1 to point 2.
-def is_between(board, x1, y1, x2, y2):
+def check_between(board, x1, y1, x2, y2):
 	#first, find the bounds of the shortest path from point 1 to point 2.
 	lower_x = x1
 	higher_x = x2
-	lower_y = y1
-	higher_y = y2
+	lower_y = y2
+	higher_y = y1
 	if (x1 > x2):
 		lower_x = x2
 		higher_x = x1
 	if (y1 > y2):
-		lower_y = y2
-		higher_y = y1
+		lower_y = y1
+		higher_y = y2
+
 	#first case: if, shortest path from point 1 to point 2 is a horizontal line,
 	#check if there is something in that line.
 	if (y1 == y2):
@@ -116,19 +119,95 @@ def is_between(board, x1, y1, x2, y2):
 			is_obstacle(x, y1, board) == True
 			for x in range(lower_x+1, higher_x)
 		)
+
 	#second case: if, shortest path from point 1 to point 2 is a vertical line,
 	#check if there is something in that line.
 	if (x1 == x2):
 		return all(
 			is_obstacle(x1, y, board) == True
-			for y in range(lower_y+1, higher_y)
+			for y in range(higher_y+1, lower_y)
 		)
+
 	slope = find_slope(x1, y1, x2, y2)
 	#third case: the points are connectable by a negative slope (top left to bottom right)
-
+	if (slope < 0):
+		#look for a cut-off path starting from the top. In this case, a cut-off path is made if it reaches the bottom or right.
+		for x in range(lower_x+1, higher_x+1):
+			if (is_obstacle(x, higher_y, board)):
+				stack, path = [[x, higher_y]], []
+				while stack:
+					point = stack.pop()
+					if point in path:
+						continue
+					if (point[0] == lower_x or point[1] ==lower_y):
+						return True
+					path.append(point)
+					if (check_downleft(point[0], point[1], board)):
+						stack.append([point[0]-1, point[1]+1])
+					#don't add the bottom right point
+					if (check_down(point[0], point[1], board) and (point[0] != higher_x and point[1]+1 != lower_y)):
+						stack.append([point[0], point[1]+1])
+					#don't add the bottom right point and stay within bounds
+					if (check_downright(point[0], point[1], board) and (point[0]+1 != higher_x and point[1]+1 != lower_y) and point[0]+1 <= higher_x): 
+						stack.append([point[0]+1, point[1]+1])
+		#look for a cut-off path starting from the left. In this case, a cut-off path is made if it reaches the bottom or right.
+		for y in range(higher_y+1, lower_y+1):
+			if (is_obstacle(lower_x, y, board)):
+				stack, path = [[lower_x, y]], []
+				while stack:
+					point = stack.pop()
+					if point in path:
+						continue
+					if (point[0] == higher_x or point[1] == higher_y):
+						return True
+					path.append(point)
+					if (check_upright(point[0], point[1], board)):
+						stack.append([point[0]+1, point[1]-1])
+					#don't add the bottom right point
+					if (check_right(point[0], point[1], board) and (point[0]+1 != higher_x and point[1] != lower_y)):
+						stack.append([point[0]+1, point[1]])
+					#don't add the bottom right point and stay within bounds
+					if (check_downright(point[0], point[1], board) and (point[0]+1 != higher_x and point[1]+1 != lower_y) and point[1]+1 <= lower_y):
+						stack.append([point[0]+1, point[1]+1])
 
 	#fourth case: the points are connectable by a positive slope (bottom left to top right)
-
-
-	return False
-"""
+	if (slope > 0):
+		#look for a cut-off path starting from the top. In this case, a cut-off path is made it reaches the right or bottom.
+		for x in range(lower_x, higher_x):
+			if (is_obstacle(x, higher_y, board)):
+				stack, path = [[x, higher_y]], []
+				while stack:
+					point = stack.pop()
+					if point in path:
+						continue
+					if (point[0] == higher_x or point[1] ==lower_y):
+						return True
+					path.append(point)
+					#don't add the bottom left point and stay within bounds
+					if (check_downleft(point[0], point[1], board) and (point[0]-1 != lower_x and point[1]+1 != lower_y) and point[0]-1 >= lower_x):
+						stack.append([point[0]-1, point[1]+1])
+					#don't add the bottom left point
+					if (check_down(point[0], point[1], board) and (point[0] != lower_x and point[1]+1 != lower_y)):
+						stack.append([point[0], point[1]+1])
+					if (check_downright(point[0], point[1], board)): 
+						stack.append([point[0]+1, point[1]+1])
+		#look for a cut-off path starting from the left. In this case, a cut-off path is made it reaches the right or bottom.
+		for y in range(higher_y, lower_y):
+			if (is_obstacle(lower_x, y, board)):
+				stack, path = [[lower_x, y]], []
+				while stack:
+					point = stack.pop()
+					if point in path:
+						continue
+					if (point[0] == higher_x or point[1] == lower_y):
+						return True
+					path.append(point)
+					#don't add the upper right point and stay within bounds
+					if (check_upright(point[0], point[1], board) and (point[0]+1 != higher_x and point[1]-1 != higher_y) and point[1]-1 <= lower_y):
+						stack.append([point[0]+1, point[1]-1])
+					#don't add the upper right point
+					if (check_right(point[0], point[1], board) and (point[0]+1 != higher_x and point[1] != higher_y)):
+						stack.append([point[0]+1, point[1]])
+					if (check_downright(point[0], point[1], board)):
+						stack.append([point[0]+1, point[1]+1])
+	return False #return False if no cut-off path is found.

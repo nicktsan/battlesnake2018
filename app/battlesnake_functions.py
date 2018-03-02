0415196cf36203c4d1f01f2e3b25aa1fdde3b66a
nicktsan <nicktsang.manhay@gmail.com>
2018-03-02 03:14:09 -0800

    implemented jump point search + bugfixing

--- app/battlesnake_functions.py
+++ app/battlesnake_functions.py
@@ -1,3 +1,6 @@
+import heapq
+import numpy as np
+import math
 #create a board and fill it with food, snakes, and our snake
 def init_board(food_list, snake_list, width, height):
 	#initialize a 2d list of 0's with width columns and height rows.
@@ -18,6 +21,8 @@ def init_board(food_list, snake_list, width, height):
 #to refer an x,y point on the board, type it as board[y][x]
 #checks if point xy is an obstacle
 def is_obstacle(x, y, board):
+	if (x < 0 or x >= len(board[0]) or y < 0 or y >= len(board)):
+		return True
 	if (board[y][x] != 'food' and board[y][x] != 0):
 		return True
 	return False
@@ -28,7 +33,7 @@ def is_obstacle(x, y, board):
 def find_slope(x1, y1, x2, y2):
 	rise = y1 - y2
 	run = (x1 - x2)
-	return (float(rise)/float(run))*-1.0 #we need to multiply by -1 because y values become larger when you go down the grid.
+	return (float(rise)/float(run))*-1.0 #we need to multiply by -1 because y values become larger when you go down the board.
 	#returns a positive or negative float, which will be our slope.
 
 #Takes in the board and our snakes head location
@@ -139,58 +144,34 @@ def checkTwoTilesAway(board, x, y):
 #check if obstacles are to the left of point. Returns True
 #if something is to the left, and False if something isn't to the left.
 def check_left(x, y, board):
-	#check for wall
-	if (x == 0):
-		return True
-	#check for snakes
 	return is_obstacle(x-1, y, board)
 
 #check if obstacles are to the right of point
 def check_right(x, y, board):
-	#check for wall
-	if (x == len(board[0])-1):
-		return True
-	#check for snakes
 	return is_obstacle(x+1, y, board)
 
 #check if obstacles are above point
 def check_up(x, y, board):
-	#check for wall
-	if (y == 0):
-		return True
-	#check for snakes
 	return is_obstacle(x, y-1, board)
 
 #check if obstacles are below point
 def check_down(x, y, board):
-	#check for wall
-	if (y == len(board)-1):
-		return True
-	#check for snakes
 	return is_obstacle(x, y+1, board)
 
 #check if obstacle is above and to the left of point
 def check_upleft(x, y, board):
-	if (x == 0 or y == 0):
-		return True
 	return is_obstacle(x-1, y-1, board)
 
 #check if obstacle is below and to the left of point
 def check_downleft(x, y, board):
-	if (x == 0 or y == len(board)-1):
-		return True
 	return is_obstacle(x-1, y+1, board)
 
 #check if obstacle is above and to the right of point
 def check_upright(x, y, board):
-	if (x == len(board[0])-1 or y == 0):
-		return True
 	return is_obstacle(x+1, y-1, board)
 
 #check if obstacle is below and to the right of point
 def check_downright(x, y, board):
-	if (x == len(board[0])-1 or y == len(board)-1):
-		return True
 	return is_obstacle(x+1, y+1, board)
 
 #calculates distance between point 1 and point 2
@@ -200,117 +181,354 @@ def calc_distance(x1, y1, x2, y2):
 	total_distance = abs(horizontal_distance) + abs(vertical_distance)
 	return total_distance
 
-#returns False if nothing is in between the shortest path from point 1 to point 2.
-#returns True if something is in between the shortest path from point 1 to point 2.
-def check_between(board, x1, y1, x2, y2):
-	#first, find the bounds of the shortest path from point 1 to point 2.
-	lower_x = x1
-	higher_x = x2
-	lower_y = y2
-	higher_y = y1
-	if (x1 > x2):
-		lower_x = x2
-		higher_x = x1
-	if (y1 > y2):
-		lower_y = y1
-		higher_y = y2
+#returns the shortest path from point A to point B in a_star. Returns a list containing a list and the length of the path.
+#The path list begins with the starting node and ends in the goal node.
+def reconstruct_path(parent, current):
+	total_path = [current]
+	while current in parent.keys():
+		current = parent[current]
+		total_path.append(current)
+	return [list(reversed(total_path)), len(total_path)]
 
-	#first case: if, shortest path from point 1 to point 2 is a horizontal line,
-	#check if there is something in that line.
-	if (y1 == y2):
-		return all(
-			is_obstacle(x, y1, board) == True
-			for x in range(lower_x+1, higher_x)
-		)
+#use this method to find a path from point A to point B. Returns None if there is no path.
+def a_star(start, goal, board):
+	#start and goal should be (x,y) objects
+	#to call this function: a_star((x0, y0), (x1, y1), board)
+	start = tuple(start)
+	goal = tuple(goal)
+	x0, y0 = start[0], start[1]
+	x1, y1 = goal[0], goal[1]
 
-	#second case: if, shortest path from point 1 to point 2 is a vertical line,
-	#check if there is something in that line.
-	if (x1 == x2):
-		return all(
-			is_obstacle(x1, y, board) == True
-			for y in range(higher_y+1, lower_y)
-		)
+	closed_set = []
+	open_set   = [start]
+	parent = {} #empty map
 
-	slope = find_slope(x1, y1, x2, y2)
-	#third case: the points are connectable by a negative slope (top left to bottom right)
-	if (slope < 0):
-		#look for a cut-off path starting from the top. In this case, a cut-off path is made if it reaches the bottom or right.
-		for x in range(lower_x+1, higher_x+1):
-			if (is_obstacle(x, higher_y, board)):
-				stack, path = [[x, higher_y]], []
-				while stack:
-					point = stack.pop()
-					if point in path:
-						continue
-					if (point[0] == lower_x or point[1] ==lower_y):
-						return True
-					path.append(point)
-					if (check_downleft(point[0], point[1], board)):
-						stack.append([point[0]-1, point[1]+1])
-					#don't add the bottom right point
-					if (check_down(point[0], point[1], board) and (point[0] != higher_x and point[1]+1 != lower_y)):
-						stack.append([point[0], point[1]+1])
-					#don't add the bottom right point and stay within bounds
-					if (check_downright(point[0], point[1], board) and (point[0]+1 != higher_x and point[1]+1 != lower_y) and point[0]+1 <= higher_x): 
-						stack.append([point[0]+1, point[1]+1])
-		#look for a cut-off path starting from the left. In this case, a cut-off path is made if it reaches the bottom or right.
-		for y in range(higher_y+1, lower_y+1):
-			if (is_obstacle(lower_x, y, board)):
-				stack, path = [[lower_x, y]], []
-				while stack:
-					point = stack.pop()
-					if point in path:
-						continue
-					if (point[0] == higher_x or point[1] == higher_y):
-						return True
-					path.append(point)
-					if (check_upright(point[0], point[1], board)):
-						stack.append([point[0]+1, point[1]-1])
-					#don't add the bottom right point
-					if (check_right(point[0], point[1], board) and (point[0]+1 != higher_x and point[1] != lower_y)):
-						stack.append([point[0]+1, point[1]])
-					#don't add the bottom right point and stay within bounds
-					if (check_downright(point[0], point[1], board) and (point[0]+1 != higher_x and point[1]+1 != lower_y) and point[1]+1 <= lower_y):
-						stack.append([point[0]+1, point[1]+1])
+	#g_score should be distance from starting point to another point (ex: a neighbour).
+	g_score = [[10000 for x in xrange(len(board[y]))] for y in xrange(len(board))]
+	#Obviously, the G score from the starting point to itself is 0.
+	g_score[y0, x0] = 0
 
-	#fourth case: the points are connectable by a positive slope (bottom left to top right)
-	if (slope > 0):
-		#look for a cut-off path starting from the top. In this case, a cut-off path is made it reaches the right or bottom.
-		for x in range(lower_x, higher_x):
-			if (is_obstacle(x, higher_y, board)):
-				stack, path = [[x, higher_y]], []
-				while stack:
-					point = stack.pop()
-					if point in path:
-						continue
-					if (point[0] == higher_x or point[1] ==lower_y):
-						return True
-					path.append(point)
-					#don't add the bottom left point and stay within bounds
-					if (check_downleft(point[0], point[1], board) and (point[0]-1 != lower_x and point[1]+1 != lower_y) and point[0]-1 >= lower_x):
-						stack.append([point[0]-1, point[1]+1])
-					#don't add the bottom left point
-					if (check_down(point[0], point[1], board) and (point[0] != lower_x and point[1]+1 != lower_y)):
-						stack.append([point[0], point[1]+1])
-					if (check_downright(point[0], point[1], board)): 
-						stack.append([point[0]+1, point[1]+1])
-		#look for a cut-off path starting from the left. In this case, a cut-off path is made it reaches the right or bottom.
-		for y in range(higher_y, lower_y):
-			if (is_obstacle(lower_x, y, board)):
-				stack, path = [[lower_x, y]], []
-				while stack:
-					point = stack.pop()
-					if point in path:
-						continue
-					if (point[0] == higher_x or point[1] == lower_y):
-						return True
-					path.append(point)
-					#don't add the upper right point and stay within bounds
-					if (check_upright(point[0], point[1], board) and (point[0]+1 != higher_x and point[1]-1 != higher_y) and point[1]-1 <= lower_y):
-						stack.append([point[0]+1, point[1]-1])
-					#don't add the upper right point
-					if (check_right(point[0], point[1], board) and (point[0]+1 != higher_x and point[1] != higher_y)):
-						stack.append([point[0]+1, point[1]])
-					if (check_downright(point[0], point[1], board)):
-						stack.append([point[0]+1, point[1]+1])
-	return False #return False if no cut-off path is found.
+	f_score = [[10000 for x in xrange(len(board[y]))] for y in xrange(len(board))]
+	f_score[y0, x0] = calc_distance(x0, y0, x1, y1)
+
+	while(len(open_set) > 0):
+		current = min(open_set, key=lambda p: f_score[p[1]][p[0]])
+
+		if (current == goal):
+			return reconstruct_path(parent, goal)
+
+		open_set.remove(current)
+		closed_set.append(current)
+
+		neighbours = []
+		if (check_up(x0, y0, board) == False):
+			neighbours.append((x0, y0-1))
+		if (check_right(x0, y0, board) == False):
+			neighbours.append((x0+1, y0))
+		if (check_down(x0, y0, board) == False):
+			neighbours.append((x0, y0+1))
+		if (check_left(x0, y0, board) == False):
+			neighbours.append((x0-1, y0))
+
+		for neighbour in neighbours:
+			if neighbour in closed_set:
+				continue
+			tentative_g_score = g_score[current[1]][current[0]] + calc_distance(current[0], current[1] ,neighbour[0], neighbour[1])
+			if neighbour not in open_set:
+				open_set.append(neighbour)
+			elif tentative_g_score >= g_score[neighbour[1]][neighbour[0]]:
+				continue
+
+			parent[neighbour] = current
+			g_score[neighbour[1]][neighbour[0]] = tentative_g_score
+			f_score[neighbour[1]][neighbour[0]] = tentative_g_score + calc_distance(neighbour[0], neighbour[1], x1, y1)
+	return None
+
+def norm_dir(cX, cY, pX, pY):
+	dX = int(math.copysign(1, cX - pX))
+	dY = int(math.copysign(1, cY - pY))
+	if cX-pX == 0:
+		dX = 0
+	if cY-pY == 0:
+		dY = 0
+	return (dX, dY)
+
+def find_neighbours(cX, cY, parent, board):
+	#parent should be an (x, y) object
+	neighbours = [] # a list of tuples aka (x, y) object
+	
+	if type(parent) != tuple:
+		for i,j in [(-1,0),(0,-1),(1,0),(0,1),(-1,-1),(-1,1),(1,-1),(1,1)]:
+			if (not is_obstacle(cX+i, cY+j, board)):
+				neighbours.append((cX+i, cY+j))
+		return neighbours
+	dX,dY = norm_dir(cX,cY,parent[0],parent[1])
+
+	#Check if moved diagonally from parent
+	if (dX != 0 and dY != 0):
+		if (not is_obstacle(cX, cY+dY, board)):
+			neighbours.append((cX, cY+dY))
+		if (not is_obstacle(cX+dX, cY, board)):
+			neighbours.append((cX+dX, cY))
+		if ((not is_obstacle(cX, cY+dY, board) or not is_obstacle(cX+dX, cY, board)) and not is_obstacle(cX+dX, cY+dY, board)):
+			neighbours.append((cX+dX, cY+dY))
+		if (is_obstacle(cX-dX, cY, board) and not is_obstacle(cX, cY+dY, board)):
+			neighbours.append((cX-dX, cY+dY))
+		if (is_obstacle(cX, cY-dY, board) and not is_obstacle(cX+dX, cY, board)):
+			neighbours.append((cX+dX, cY-dY))
+
+	if (dX == 0 and dY != 0):
+		if not is_obstacle(cX+dX, cY, board):
+			if not is_obstacle(cX, cY+dY, board):
+				neighbours.append((cX, cY+dY))
+			if check_right(cX, cY, board):
+				neighbours.append((cX+1, cY+dY))
+			if check_left(cX, cY, board):
+				neighbours.append((cX+dX, cY-1))
+
+	if (dX != 0 and dY == 0):
+		if not is_obstacle(cX+dX, cY, board):
+			if not is_obstacle(cX+dX, cY, board):
+				neighbours.append((cX+dX, cY))
+			if check_down(cX, cY, board):
+				neighbours.append((cX+dX,cY+1))
+			if check_up(cX, cY, board):
+				neighbours.append((cX+dX,cY-1))
+	return neighbours
+
+
+def jump(cX, cY, dX, dY, goal, board):
+	#goal should be an (x, y) object
+	#cX, cY = current point coords
+	#dX, dY = direction vectors
+	nextX = cX + dX
+	nextY = cY + dY
+	nextX2 = nextX + dX
+	nextY2 = nextY + dY
+	#check if next point is obstacle or out of bounds
+	if (is_obstacle(nextX, nextY, board)):
+		return None 
+
+	#check if next point is the goal when moving diagonally
+	if (dX != 0 and dY != 0):
+		if (nextX == goal[0] and nextY == goal[1]):
+			if (is_obstacle(nextX, cY, board) == False or is_obstacle(cX, nextY, board) == False):
+				return (nextX, nextY)
+	#else if the next point is the goal when moving orthogonally
+	elif (nextX == goal[0] and nextY == goal[1]):
+		return (nextX, nextY)
+
+	#check if direction is diagonal
+	if (dX != 0 and dY != 0):
+		#check if goal is adjacent to next
+		if ((nextX+dX, nextY) == goal or (nextX, nextY+dY) == goal):
+			return (nextX, nextY)
+		if ((not is_obstacle(nextX-dX, nextY+dY, board) and is_obstacle(nextX-dX, nextY, board)) or (not is_obstacle(nextX+dX, nextY-dY, board) and is_obstacle(nextX, nextY-dY, board))):
+			return (nextX, nextY)
+		if (jump(nextX, nextY, dX, 0, goal, board) != None or jump(nextX, nextY, 0, dY, goal, board) != None):
+			return (nextX, nextY)
+		if (is_obstacle(nextX2, nextY2, board)):
+			return None
+		if (is_obstacle(nextX2+dX, nextY2, board) and is_obstacle(nextX2, nextY2+dY, board)):
+			return None
+		if (nextX2, nextY2) == goal:
+			return (nextX2, nextY2)
+
+	#check if direction is horizontal
+	if (dX != 0 and dY == 0):
+		#check if goal is above or below the next point. If it is, return next point as a jump point.
+		if ((nextX, nextY-1) == goal or (nextX, nextY-1) == goal):
+			return (nextX, nextY)
+		if check_up(nextX, nextY, board) and not check_up(nextX+dX, nextY, board):
+			return (nextX, nextY)
+		if check_down(nextX, nextY, board) and not check_down(nextX+dX, nextY, board):
+			return (nextX, nextY)
+		if (is_obstacle(nextX2, nextY, board)):
+			return None
+		if ((nextX2, nextY) == goal):
+			return (nextX2, nextY)
+
+	#check if direction is vertical
+	if (dX == 0 and dY != 0):
+		#check if goal is to the left or right of the next point. If it is, return next point as a jump point.
+		if ((nextX-1, nextY) == goal or (nextX+1, nextY) == goal):
+			return (nextX, nextY)
+		if check_left(nextX, nextY, board) and not check_left(nextX, nextY+dY, board):
+			return (nextX, nextY)
+		if check_right(nextX, nextY, board) and not check_right(nextX, nextY+dY, board):
+			return (nextX, nextY)
+		if (is_obstacle(nextX, nextY2, board)):
+			return None
+		if ((nextX, nextY2) == goal):
+			return (nextX, nextY2)
+	
+	return jump(nextX, nextY, dX, dY, goal, board)
+
+#finds successors for jump point search.
+def find_successors(cX, cY, parent, goal, board):
+	successors = []
+	#find non-obstacle neighbours
+	print "current: (%i, %i)" % (cX, cY)
+	print "parent: "
+	print parent.get((cX, cY), 0)
+	neighbours = find_neighbours(cX, cY, parent.get((cX, cY), 0), board)
+	print "neighbours: "
+	print neighbours
+
+	for neighbour in neighbours:
+		dX = neighbour[0] - cX
+		dY = neighbour[1] - cY
+		
+		print "direction: (%i, %i)" % (dX, dY)
+
+		jumpPoint = jump(cX, cY, dX, dY, goal, board)
+		if jumpPoint:
+			successors.append(jumpPoint)
+			print "jumpPoint"
+			print jumpPoint
+	return successors #should return a list of (x, y) objects
+
+
+#jump point search algorithm. Rather than examining every neighbour, find jump points
+#to cut down on path symmetry and significantly speed up the process of path searching.
+#Returns None if no path found.
+def jps(start, goal, board):
+	#start and goal should be (x,y) objects
+	#to call this function: a_star((x0, y0), (x1, y1), board)
+	start = tuple(start)
+	goal = tuple(goal)
+	x0, y0 = start[0], start[1]
+	x1, y1 = goal[0], goal[1]
+
+	if (is_obstacle(x0, y0, board) or is_obstacle(x1, y1, board)):
+		return None
+
+	closed_set = set()
+	parent = {} #empty map
+	gscore = {start:0}
+	fscore = {start:calc_distance(x0, y0, x1, y1)}
+	pqueue = []
+
+	heapq.heappush(pqueue, (fscore[start], start))
+	while(pqueue):
+		current = tuple(heapq.heappop(pqueue)[1])
+		simple_grid[current[1]][current[0]] = 3
+		print "updated simple grid"
+		print_grid()
+		
+		if (current == goal):
+			return reconstruct_path(parent, goal)
+
+		closed_set.add(current)
+		successors = find_successors(current[0], current[1], parent, goal, board)
+		
+		for successor in successors:
+			jumpPoint = successor
+
+			if jumpPoint in closed_set:
+				continue
+			tentative_gscore = gscore[current] + calc_distance(current[0], current[1], jumpPoint[0], jumpPoint[1])
+			print "tentative_gscore: %i" % tentative_gscore
+			if (tentative_gscore < gscore.get(jumpPoint, 0) or jumpPoint not in [j[1] for j in pqueue]):
+				parent[jumpPoint] = current
+				gscore[jumpPoint] = tentative_gscore
+				fscore[jumpPoint] = tentative_gscore + calc_distance(jumpPoint[0], jumpPoint[1], goal[0], goal[1])
+				heapq.heappush(pqueue, (fscore[jumpPoint], jumpPoint))
+
+	return None
+
+def print_grid():
+	for row in simple_grid:
+		for e in row:
+			print e,
+		print
+"""
+food_spawn= {'data': 
+			[
+				{
+				'object': 'point',
+				'x': 0,
+				'y': 9
+
+				}
+			],	
+			'object': 'list'
+			}
+snake_spawn= {
+	"data": [
+	  {
+		"body": {
+		  "data": [
+			{
+			  "object": "point",
+			  "x": 1,
+			  "y": 8
+			},
+			{
+			  "object": "point",
+			  "x": 1,
+			  "y": 9
+			},
+			{
+			  "object": "point",
+			  "x": 1,
+			  "y": 10
+			}
+		  ],
+		  "object": "list"
+		},
+		"health": 100,
+		"id": "58a0142f-4cd7-4d35-9b17-815ec8ff8e70",
+		"length": 3,
+		"name": "Sonic Snake",
+		"object": "snake",
+		"taunt": "Gotta go fast"
+	  },
+	  {
+		"body": {
+		  "data": [
+			{
+			  "object": "point",
+			  "x": 6,
+			  "y": 15
+			},
+			{
+			  "object": "point",
+			  "x": 7,
+			  "y": 15
+			},
+			{
+			  "object": "point",
+			  "x": 8,
+			  "y": 15
+			}
+		  ],
+		  "object": "list"
+		},
+		"health": 100,
+		"id": "48ca23a2-dde8-4d0f-b03a-61cc9780427e",
+		"length": 3,
+		"name": "Typescript Snake",
+		"object": "snake",
+		"taunt": ""
+	  }
+	],
+	"object": "list"
+  }
+grid = init_board(food_spawn, snake_spawn, 20, 20)
+
+simple_grid = np.zeros((20, 20), dtype=int)
+for h in range(0, 20):
+	for w in range(0, 20):
+		if grid[h][w] == 'food':
+			simple_grid[h][w] = 2
+		if (grid[h][w] != 'food' and grid[h][w] != 0):
+			simple_grid[h][w] = 1
+
+simple_grid[9][19] = 3
+
+#print_grid()
+print 
+
+print jps((19, 9), (0, 9), grid)
+"""

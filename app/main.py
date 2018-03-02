79c9e5ec942451f7ab0bc41f849946fa6040f172
Kaito2008 <Kurankaname@126.com>
2018-03-02 15:35:44 -0800

    seek_food

--- app/main.py
+++ app/main.py
@@ -77,6 +77,10 @@ def start():
 @bottle.post('/move')
 def move():
 	data = bottle.request.json
+
+	for f in data['food']:
+        grid[f[0]][f[1]] = FOOD
+        
 	game_id = data.get('game_id')
 	board_width = data.get('width')
 	board_height = data.get('height')
@@ -90,26 +94,83 @@ def move():
 	direction = 'up'
 	#direction = random.choice(directions)
 
-	if (mysnake_head['y'] == 0 and mysnake_head['x'] == 0):   #top left
-		direction = 'down'
-	elif (mysnake_head['y'] == board_height-1 and mysnake_head['x'] == board_width-1):   #bottom right
-		direction = 'up'
-	elif (mysnake_head['y'] == 0 and mysnake_head['x'] == board_width-1 ):    #top right
-		direction = 'left'
-	elif (mysnake_head['y'] == board_height-1 and mysnake_head['x'] == 0):   #bottom left
-		direction = 'right'
-	elif (mysnake_head['y'] == 0):   #top
-		direction = 'left'
-	elif (mysnake_head['x'] == 0):   #left
-		direction = 'down'
-	elif (mysnake_headp['y'] == board_height-1):    #bottom
-		direction = 'right'
-	elif (mysnake_head['x'] == board_width-1):     #right
-		direction = 'up'
+	#Find our head location
+	x2 = mysnake_head['x'],
+	y2 = mysnake_head['y']
+	coordinate = []
+	for food in food_list['data']:    # Find a food
+		x1 = food['x'],
+		y1 = food['y']
+		# check all obstacles in between (call function)
+		distance = calc_distance(x1,y1,x2,y2)   #calculate the distance from food to head
+		# get the coordinate of all other snakes
+		# call obstacles in between function
+		# call calc_distance function
+
+		#compare our distance with their distance
+		# if they are closer, abandon the food, find next food and go through the same process 
+
+		# got the food that we are closer than others, store it in list
+		coordinate.append([distance, x1, y1])	    #store distancea and coordinatea in list
+	coordinate = sorted(coordinate, key=lambda x: x[0])   	#sort all the coordinate base on distance
+	for row in coordinate:					#choose row 0 as the closest food
+		food_dist = row[0]
+		x1 = row[1]
+		y1 = row[2]
+	
+	#figure out which way to turn
+	if (x1 > x2):       # if food is on right hand side
+		if (y1 > y2):    # if food is down-right
+			direction = 'down'
+		elif(y1 < y2):   # if food is up-right
+			direction = 'up'
+		else:
+			direction = 'right'
+	elif (x1 < x2):      # food is on the left hand side
+		if (y1 > y2):    #food is down-left
+			direction = 'down'
+		elif(y1 < y2):   #food is up-left
+			direction = 'up'
+		else:
+			direction = 'left'
+	else:
+		if (y1 > y2):    #food is directly below
+			direction = 'down'
+		else:            #food is directly above
+			direction = 'up'		
 	return {
 		'move': direction,
 		'taunt': 'dat is not de wae'
-	}
+	}		
+#sfwefw
+
+	# Find the distance from us to food
+	# Find the distance from opponent to food
+	# If the opponent is closer then we pass
+	# If we are closer then find the next food and compare
+
+	#if (mysnake_head['y'] == 0 and mysnake_head['x'] == 0):   #top left
+	#	direction = 'down'
+	#elif (mysnake_head['y'] == board_height-1 and mysnake_head['x'] == board_width-1):   #bottom right
+	#	direction = 'up'
+	#elif (mysnake_head['y'] == 0 and mysnake_head['x'] == board_width-1 ):    #top right
+	#	direction = 'left'
+	#elif (mysnake_head['y'] == board_height-1 and mysnake_head['x'] == 0):   #bottom left
+	#	direction = 'right'
+	#elif (mysnake_head['y'] == 0):   #top
+	#	direction = 'left'
+	#elif (mysnake_head['x'] == 0):   #left
+	#	direction = 'down'
+	#elif (mysnake_headp['y'] == board_height-1):    #bottom
+	#	direction = 'right'
+	#elif (mysnake_head['x'] == board_width-1):     #right
+	#	direction = 'up'
+	#return {
+	#	'move': direction,
+	#	'taunt': 'dat is not de wae'
+	#}
+
+
 
 
 # Expose WSGI app (so gunicorn can find it)
@@ -121,3 +182,5 @@ if __name__ == '__main__':
 		host=os.getenv('IP', '0.0.0.0'),
 		port=os.getenv('PORT', '8080'),
 debug = True)
+
+ food = board[y][x] 

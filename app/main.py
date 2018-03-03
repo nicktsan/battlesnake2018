4e522f2702512580981bb8cf7344eb714a29675c
Kaito2008 <Kurankaname@126.com>
2018-03-03 02:20:40 -0800

    debig

--- app/main.py
+++ app/main.py
@@ -128,28 +128,43 @@ def move():
 	food_dist = row[0]
 	x1 = row[1]
 	y1 = row[2]
+
+	is_left = check_left(mysnake_head['x'], mysnake_head['y'], board)
+	is_right = check_right(mysnake_head['x'], mysnake_head['y'], board)
+	is_up = check_up(mysnake_head['x'], mysnake_head['y'], board)
+	is_down = check_down(mysnake_head['x'], mysnake_head['y'], board)
+	
+	# TODO: Do things with data
+	if (is_up == True):
+		directions.remove('up')
+	if (is_down == True):
+		directions.remove('down')
+	if (is_left == True):
+		directions.remove('left')
+	if (is_right == True):
+		directions.remove('right')
 	
 	
 	#figure out which way to turn
 	if (x1 > x2):       # if food is on right hand side
 		if (y1 > y2):    # if food is down-right
-			direction = 'down'
+			direction = random.choice(directions)
 		elif(y1 < y2):   # if food is up-right
-			direction = 'up'
+			direction = random.choice(directions)
 		else:
-			direction = 'right'
+			direction = random.choice(directions)
 	elif (x1 < x2):      # food is on the left hand side
 		if (y1 > y2):    #food is down-left
-			direction = 'down'
+			direction = random.choice(directions)
 		elif(y1 < y2):   #food is up-left
-			direction = 'up'
+			direction = random.choice(directions)
 		else:
-			direction = 'left'
+			direction = random.choice(directions)
 	else:
 		if (y1 > y2):    #food is directly below
-			direction = 'down'
+			direction = random.choice(directions)
 		else:            #food is directly above
-			direction = 'up'      
+			direction = random.choice(directions)   
 	
 	return {
 		'move': direction,

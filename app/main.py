1536f3e5a42d99cc6b1dd193de6843ad7a335747
Kaito2008 <Kurankaname@126.com>
2018-03-02 20:29:05 -0800

    debug

--- app/main.py
+++ app/main.py
@@ -86,7 +86,7 @@ def move():
 		
 		for other_snake in snake_list['data']:
 			if (other_snake['id'] != mysnake['id']):
-				#othersnake_head = othersnake['data']['body']['data'][0]
+				othersnake_head = other_snake['body']['data'][0]
 				#x3 = othersnake_head['x']
 				#y3 = othersnake_head['y']
 				#other_distance = calc_distance(x1,y1,x3,y3)

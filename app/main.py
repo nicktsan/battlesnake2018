22e55f52e5e9ade1928aafcc75507d66c54beef1
Kaito2008 <Kurankaname@126.com>
2018-03-02 18:28:31 -0800

    add other snake

--- app/main.py
+++ app/main.py
@@ -74,44 +74,71 @@ def move():
 	x2 = mysnake_head['x']
 	y2 = mysnake_head['y']
 	coordinate = []
+	
 	for food in food_list['data']:    # Find a food
-	    x1 = food['x']
-	    y1 = food['y']
-        # check all obstacles in between (call function)
-	    distance = calc_distance(x1,y1,x2,y2)   #calculate the distance from food to head
-        # get the coordinate of all other snakes
-        # call obstacles in between function
-        # call calc_distance function
+		x1 = food['x']
+		y1 = food['y']
+		food_ok = 0
+		# check all obstacles in between (call function)
+		distance = calc_distance(x1,y1,x2,y2)   #calculate the distance from food to head
+		# get the coordinate of all other snakes
+		for other_snake in snake_list['data']:
+			if (snake_list['id'] == mysnake['id']):
+				continue
+			else:
+				othersnake_head = othersnake['body']['data'][0]
+				x3 = othersnake_head['x']
+				y3 = othersnake_head['y']
+				other_distance = calc_distance(x1,y1,x3,y3)
+				if (distance < other_distance):
+					pass
+				elif (distance > other_distance):
+					food_ok + 1
+				else:
+					other_length = othersnake['length']
+					our_length = mysnake['length']
+					if (other_length > our_length):
+						food_ok + 1
+					elif (our_length > other_length):
+						pass
+					else:
+						food_ok + 1
+		if (food_ok == 0):
+			coordinate.append([distance, x1, y1])       #store distancea and coordinatea in list
+				
+		# call obstacles in between function
+		# call calc_distance function
 
-        #compare our distance with their distance
-        # if they are closer, abandon the food, find next food and go through the same process 
+		#compare our distance with their distance
+		# if they are closer, abandon the food, find next food and go through the same process 
 
-        # got the food that we are closer than others, store it in list
-	    coordinate.append([distance, x1, y1])       #store distancea and coordinatea in list
+		# got the food that we are closer than others, store it in list
+	   
+	   
 	coordinate = sorted(coordinate, key=lambda x:x[0])     #sort all the coordinate base on distance
 	row = coordinate[0]             #choose row 0 as the closest food
 	food_dist = row[0]
 	x1 = row[1]
 	y1 = row[2]
-    #
-    #figure out which way to turn
+	#
+	#figure out which way to turn
 	if (x1 > x2):       # if food is on right hand side
 		if (y1 > y2):    # if food is down-right
-	  		direction = 'down'
+			direction = 'down'
 		elif(y1 < y2):   # if food is up-right
-	   		direction = 'up'
+			direction = 'up'
 		else:
-		   	direction = 'right'
+			direction = 'right'
 	elif (x1 < x2):      # food is on the left hand side
 		if (y1 > y2):    #food is down-left
 			direction = 'down'
 		elif(y1 < y2):   #food is up-left
-	   		direction = 'up'
+			direction = 'up'
 		else:
 			direction = 'left'
 	else:
 		if (y1 > y2):    #food is directly below
-	   		direction = 'down'
+			direction = 'down'
 		else:            #food is directly above
 			direction = 'up'      
 	#is_left = check_left(mysnake_head['x'], mysnake_head['y'], board)
@@ -121,31 +148,31 @@ def move():
 	
 	# TODO: Do things with data
 	#if (is_up == True):
-	#	directions.remove('up')
+	#   directions.remove('up')
 	#if (is_down == True):
-	#	directions.remove('down')
+	#   directions.remove('down')
 	#if (is_left == True):
-	#	directions.remove('left')
+	#   directions.remove('left')
 	#if (is_right == True):
-	#	directions.remove('right')
+	#   directions.remove('right')
 	#direction = random.choice(directions)
 
 	#We only need to do advanced decision making if there is more than 1 viable choice that will not kill the snake.
 	#if (len(directions) > 1):
 		#make a points list for the length of remaining viable directions.
-	#	points = np.zeros(len(directions)) #np.zeros(x) creates an array of zeros of length x.
+	#   points = np.zeros(len(directions)) #np.zeros(x) creates an array of zeros of length x.
 		
 		#TODO assign points to corresponding choices
 
 
 
 		#calculate which remaining option has the most points
-	#	max_point_index = np.argwhere(points == np.amax(points))
-	#	top_dirs = []
-	#	#create a list of indeces of highest points.
-	#	for index in max_point_index:
-	#		top_dirs.append(directions[index[0]])
-	#	direction = random.choice(top_dirs)
+	#   max_point_index = np.argwhere(points == np.amax(points))
+	#   top_dirs = []
+	#   #create a list of indeces of highest points.
+	#   for index in max_point_index:
+	#       top_dirs.append(directions[index[0]])
+	#   direction = random.choice(top_dirs)
 	return {
 		'move': direction,
 		'taunt': 'dat is not de wae'

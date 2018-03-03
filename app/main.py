bad13e45754247e8de739d9fcea9b12fb43a858c
Kaito2008 <Kurankaname@126.com>
2018-03-02 20:16:36 -0800

    debug

--- app/main.py
+++ app/main.py
@@ -84,7 +84,7 @@ def move():
 		distance = calc_distance(x1,y1,x2,y2)   #calculate the distance from food to head
 		# get the coordinate of all other snakes
 		
-		for other_snake in snake_list['data']:
+		#for other_snake in snake_list['data']:
 			if (other_snake['id'] != mysnake['id']):
 				#othersnake_head = othersnake['data']['body']['data'][0]
 				#x3 = othersnake_head['x']

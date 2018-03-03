83dac353b0d343d7831bdfc823835dd0a4ac0136
Kaito2008 <Kurankaname@126.com>
2018-03-03 09:33:07 -0800

    debug

--- app/battlesnake_functions.py
+++ app/battlesnake_functions.py
@@ -59,7 +59,7 @@ def seek_food(mysnake_head, food_list, snake_list, mysnake):
 					continue
 				elif (distance > other_distance):
 					food_ok = False
-					
+
 				else:
 					other_length = other_snake['length']
 					our_length = mysnake['length']

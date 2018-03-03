7ad7d02ef89ce586e807b352a7bc9934753c8820
Kaito2008 <Kurankaname@126.com>
2018-03-02 22:54:51 -0800

    debug

--- app/battlesnake_functions.py
+++ app/battlesnake_functions.py
@@ -48,8 +48,9 @@ def seek_food(mysnake_head, food_list, snake_list, mysnake):
 		# check all obstacles in between (call function)
 		distance = calc_distance(x1,y1,x2,y2)   #calculate the distance from food to head
 		# get the coordinate of all other snakes
-		"""
+		
 		for other_snake in snake_list['data']:
+			"""
 			if (other_snake['id'] != mysnake['id']):
 				othersnake_head = other_snake['body']['data'][0]
 				x3 = othersnake_head['x']

0503391fb5d1cabc47c25a34db8156e2e1116ff4
Kaito2008 <Kurankaname@126.com>
2018-03-02 16:42:30 -0800

    debug

--- app/main.py
+++ app/main.py
@@ -71,38 +71,81 @@ def move():
 
 	board = init_board(food_list, snake_list, board_width, board_height)
 	
-	is_left = check_left(mysnake_head['x'], mysnake_head['y'], board)
-	is_right = check_right(mysnake_head['x'], mysnake_head['y'], board)
-	is_up = check_up(mysnake_head['x'], mysnake_head['y'], board)
-	is_down = check_down(mysnake_head['x'], mysnake_head['y'], board)
+	x2 = mysnake_head['x'],
+	y2 = mysnake_head['y']
+	coordinate = []
+	for food in food_list['data']:    # Find a food
+	    x1 = food['x'],
+	    y1 = food['y']
+        # check all obstacles in between (call function)
+	    distance = calc_distance(x1,y1,x2,y2)   #calculate the distance from food to head
+        # get the coordinate of all other snakes
+        # call obstacles in between function
+        # call calc_distance function
+
+        #compare our distance with their distance
+        # if they are closer, abandon the food, find next food and go through the same process 
+
+        # got the food that we are closer than others, store it in list
+	    coordinate.append([distance, x1, y1])       #store distancea and coordinatea in list
+	coordinate = sorted(coordinate, key=lambda x: x[0])     #sort all the coordinate base on distance
+	row = coordinate[0]             #choose row 0 as the closest food
+	food_dist = row[0]
+	x1 = row[1]
+	y1 = row[2]
+    
+    #figure out which way to turn
+	if (x1 > x2):       # if food is on right hand side
+	    if (y1 > y2):    # if food is down-right
+	        direction = 'down'
+	    elif(y1 < y2):   # if food is up-right
+	        direction = 'up'
+	    else:
+	        direction = 'right'
+	elif (x1 < x2):      # food is on the left hand side
+	    if (y1 > y2):    #food is down-left
+	        direction = 'down'
+	    elif(y1 < y2):   #food is up-left
+	        direction = 'up'
+	    else:
+	        direction = 'left'
+	else:
+	    if (y1 > y2):    #food is directly below
+	        direction = 'down'
+	    else:            #food is directly above
+	        direction = 'up'      
+	#is_left = check_left(mysnake_head['x'], mysnake_head['y'], board)
+	#is_right = check_right(mysnake_head['x'], mysnake_head['y'], board)
+	#is_up = check_up(mysnake_head['x'], mysnake_head['y'], board)
+	#is_down = check_down(mysnake_head['x'], mysnake_head['y'], board)
 	
 	# TODO: Do things with data
-	if (is_up == True):
-		directions.remove('up')
-	if (is_down == True):
-		directions.remove('down')
-	if (is_left == True):
-		directions.remove('left')
-	if (is_right == True):
-		directions.remove('right')
-	direction = random.choice(directions)
+	#if (is_up == True):
+	#	directions.remove('up')
+	#if (is_down == True):
+	#	directions.remove('down')
+	#if (is_left == True):
+	#	directions.remove('left')
+	#if (is_right == True):
+	#	directions.remove('right')
+	#direction = random.choice(directions)
 
 	#We only need to do advanced decision making if there is more than 1 viable choice that will not kill the snake.
-	if (len(directions) > 1):
+	#if (len(directions) > 1):
 		#make a points list for the length of remaining viable directions.
-		points = np.zeros(len(directions)) #np.zeros(x) creates an array of zeros of length x.
+	#	points = np.zeros(len(directions)) #np.zeros(x) creates an array of zeros of length x.
 		
 		#TODO assign points to corresponding choices
 
 
 
 		#calculate which remaining option has the most points
-		max_point_index = np.argwhere(points == np.amax(points))
-		top_dirs = []
-		#create a list of indeces of highest points.
-		for index in max_point_index:
-			top_dirs.append(directions[index[0]])
-		direction = random.choice(top_dirs)
+	#	max_point_index = np.argwhere(points == np.amax(points))
+	#	top_dirs = []
+	#	#create a list of indeces of highest points.
+	#	for index in max_point_index:
+	#		top_dirs.append(directions[index[0]])
+	#	direction = random.choice(top_dirs)
 	return {
 		'move': direction,
 		'taunt': 'dat is not de wae'

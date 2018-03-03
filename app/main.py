037a5b088889cf715a5c06aedd665d41440f7164
Kaito2008 <Kurankaname@126.com>
2018-03-03 00:37:41 -0800

    debug

--- app/main.py
+++ app/main.py
@@ -126,7 +126,7 @@ def move():
 	y1 = row[2]
 	"""
 	#coordinate,x2,y2 = seek_food(mysnake_head, food_list, snake_list, mysnake, othersnake_tail)
-	coordinate,x2,y2 = seek_food(mysnake_head, mysnake, othersnake_tail)
+	coordinate,x2,y2 = seek_food(mysnake_head, othersnake_tail)
 	"""
 	row = coordinate[0]             #choose row 0 as the closest food
 	food_dist = row[0]
